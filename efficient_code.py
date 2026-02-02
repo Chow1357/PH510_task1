9ffab69e2c8f1f45aeece4743de370a24d93f575
Callum Howie <kwb21127@archie-s.hpc.strath.ac.uk>
2026-02-02 16:38:45 +0000

    imported functions, defined MPI features.

--- efficient_code.py
+++ efficient_code.py
@@ -1,1 +1,17 @@
 #!/opt/software/anaconda/python-3.10.9/bin/python
+"""
+approximation of pi using th emid point rule for a definite integral 
+
+Tested using: 
+    Python 3.10.9
+    mpi4py
+    OpenMPI
+"""
+#import the necessary functions 
+import mpi4py as MPI 
+import numpy as np 
+
+# stating defining variables for the main features of MPI including the main communicator 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nproc = comm.Get_size() 
