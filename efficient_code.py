eb78b0b34ac583c649c4ad62d3a2f6f2a12d5c56
Callum Howie <kwb21127@archie-s.hpc.strath.ac.uk>
2026-02-02 16:48:56 +0000

    Please enter the commit message for your changes. Lines starting number of integration points, sample width. balanced workload

--- efficient_code.py
+++ efficient_code.py
@@ -15,3 +15,11 @@ import numpy as np
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 nproc = comm.Get_size() 
+
+# number of sample points in the integration and defining the width of each sample point between 0 and 1
+N = 100000000
+DELTA = 1.0 / N 
+
+#sharing the workload, number of integration points, equally between the cores (nproc = 16) 
+min_jobs = N // nproc 
+
