c79ca2a6b2a3b5ef23ca9e807bd1bc523d955637
Callum Howie <kwb21127@archie-t.hpc.strath.ac.uk>
2026-02-02 23:56:31 +0000

    deleting white space

--- efficient_code.py
+++ efficient_code.py
@@ -1,13 +1,14 @@
 #!/opt/software/anaconda/python-3.10.9/bin/python
 """
-code that approximates pi using the mid point rule for a definite integral, optimised for a parallel environment
+code that approximates pi using the mid point rule
+for a definite integral, optimised for a parallel environment
 
-Tested using: 
+Tested using:
     Python 3.10.9
     mpi4py
     OpenMPI
 """
-#import the necessary functions 
+#import the necessary functions
 from mpi4py import MPI # pylint: disable=no-name-in-module
 import numpy as np
 #stating defining variables for the main features of MPI
@@ -16,8 +17,8 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 nproc = comm.Get_size()
 
-# number of sample points in the integration and defining the width
-# of each sample point between 0 and 1
+#number of sample points in the integration and defining the width
+#of each sample point between 0 and 1
 N = 100000000
 DELTA = 1.0 / N
 
@@ -29,8 +30,8 @@ min_jobs = N // nproc
 #good incase N value changes
 int_remain = N % nproc
 local_n = rank * min_jobs(1 if rank < int_remain else 0)
-#ensures each MPI process indexes at the correct sampling point so each is
-#only sampled once and then stops at the correct place
+#ensures each MPI process indexes at the correct sampling point
+#so each is only sampled once and then stops at the correct place
 start = rank * min_jobs + min(rank, int_remain)
 end = start + local_n
 
@@ -38,7 +39,7 @@ end = start + local_n
 i = np.arange(start, end, dtype=np.float64)
 x = (x + 0.5) * DELTA
 local_sum = np.sum(4.0 / (1.0 + x*x), dtype=np.float64)
-#combines all the partial results in each rank using the SUM operation 
+#combines all the partial results in each rank using the SUM operation
 #and then returns this result only to rank 0
 I = comm.reduce(local_sum, op=MPI.SUM, root=0)
 #prints the final result in rank 0 to 10 decimal places
