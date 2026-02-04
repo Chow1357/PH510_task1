c592edf0abcd0815cd44ef261dfffc522bf29fe5
Callum Howie <kwb21127@archie-t.hpc.strath.ac.uk>
2026-02-04 00:46:59 +0000

    j Please enter the commit message for your changes. Lines starting

--- run_code_mpi_only_best_efficient_allcores.sh
+++ run_code_mpi_only_best_efficient_allcores.sh
@@ -25,7 +25,7 @@
 module load mpi 
 
 # Modify the line below to run your program  python3 Task1-code.py
-for i in 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 
 do
     echo "===== Running with $i MPI ranks ====="
     
