e26223a882a92e244d2c14bb4ad6ec6aeaa27886
Callum Howie <kwb21127@archie-t.hpc.strath.ac.uk>
2026-02-05 02:18:38 +0000

    added shebang and precursor to code

---
+++ efficient_code_forloop.py
@@ -0,0 +1,9 @@
+#!/opt/software/anaconda/python-3.10.9/bin/python
+"""
+Parallel approximation of pi using MPI and the mid-point rule
+
+Tested with: 
+    Python 3.10.9
+    mpi4py 
+    OpenMPI
+"""
