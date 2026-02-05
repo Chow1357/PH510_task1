4804aeb1ee0959081c7e1056ef006a93c8429b1e
Callum Howie <kwb21127@archie-t.hpc.strath.ac.uk>
2026-02-05 02:24:15 +0000

    defining number of integral samples we are using

--- efficient_code_forloop.py
+++ efficient_code_forloop.py
@@ -15,3 +15,8 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 nproc = comm.Get_size()
+#N is the number of integral samples
+#delta is thw idth of each integral sample point
+N = 100000000
+DELTA = 1.0 / N
+#defining the integral
