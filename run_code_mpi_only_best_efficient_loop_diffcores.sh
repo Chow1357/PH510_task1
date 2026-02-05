3ddb51a68947ece72bfb429c40f449c032f11f8a
Callum Howie <kwb21127@archie-t.hpc.strath.ac.uk>
2026-02-05 02:40:54 +0000

    job script for running efficiet code with for loop ad orginal codeÂ

---
+++ run_code_mpi_only_best_efficient_loop_diffcores.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Propagate environment variables to the compute node
+#SBATCH --export=ALL
+
+# Run in the standard partition (queue)
+#SBATCH --partition=teaching
+
+# Distribute processes in round-robin fashion
+#SBATCH --distribution=block:block
+
+# No of cores required (max. of 16, 4GB RAM per core)
+#SBATCH --ntasks=16 
+
+# Runtime (hard, HH:MM:SS)
+#SBATCH --time=24:00:00
+
+# Job name
+#SBATCH --job-name=integral 
+
+# Output file
+#SBATCH --output=slurm-%j.out
+
+# choose which version to load
+module load mpi 
+
+# Modify the line below to run your program  python3 Task1-code.py
+mpirun -np 16 python ./efficient_code_test.py >/dev/null
+
+for i in 1 2 4 8 16
+do
+    echo "===== Running with $i MPI ranks ====="
+    
+    perf stat -e cycles,instructions,cache-misses mpirun -np $i ./efficient_code_test.py
+    perf stat -e cycles,instructions,cache-misses mpirun -np $i ./original_bad_code.py
+    
+    echo 
+done
+
+
